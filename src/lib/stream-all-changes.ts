dbfd6916fc90b59879455fba95a20e1152d27e1d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-03-19 06:14:27 -0500

    Stream content into file

--- src/lib/stream-all-changes.ts
+++ src/lib/stream-all-changes.ts
@@ -1,41 +1,28 @@
-export const streamAllChanges = async () => {
-  fetch("https://replicate.npmjs.com/_changes")
-    .then((response) => {
-      const reader = response.body.getReader();
-      return new ReadableStream({
-        start(controller) {
-          function push() {
-            reader.read().then(({ done, value }) => {
-              if (done) {
-                controller.close();
-                return;
-              }
-              controller.enqueue(value);
-              push();
-            });
-          }
-          push();
-        },
-      });
-    })
-    .then((stream) => {
-      // read the stream and write to std ourt
-      const reader = stream.getReader();
-      const decoder = new TextDecoder();
-      function read() {
-        reader.read().then(({ done, value }) => {
-          if (done) {
-            console.log("Stream finished");
-            return;
-          }
-          const text = decoder.decode(value);
-          console.log(text);
-          read();
-        });
-      }
-      read();
-    })
-    .catch((error) => {
-      console.error("Error streaming data:", error);
-    });
-};
+const response = await fetch("https://replicate.npmjs.com/_changes");
+const reader = response.body.getReader();
+
+const appendToFile = async (str: string) => {
+  const fs = await import("node:fs/promises");
+  const filePath = "./all-package-names.txt";
+  await fs.appendFile(filePath, str, "utf8");
+}
+
+// read the stream and write to std ourt
+const decoder = new TextDecoder();
+function read() {
+  reader.read().then(({ done, value }) => {
+    if (done) {
+      console.log("Stream finished");
+      return;
+    }
+    const text = decoder.decode(value);
+    appendToFile(text);
+    read();
+  }).catch((error) => {
+    console.error("Error reading stream:", error);
+  })
+}
+
+read();
+
+export {}
